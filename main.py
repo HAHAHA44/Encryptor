80338e3ec9ac80b07af1ef0e2f5ea24220bbdb1c
HAHAHA44 <603938017@qq.com>
2018-12-11 17:41:34 +0800

    des and caesar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter.messagebox as messagebox
+import des
 
 class Application(Frame):
     def __init__(self, languages=Listbox(), master=None):
@@ -13,13 +14,17 @@ class Application(Frame):
         self.hint = Label(self,text = '输入要加密的明文:')
         self.Input = Entry(self)
         self.CaserButton = Button(self, text='Caesar', command=self.CaesarShow)
+        self.DesButton = Button(self,text='Des', command=self.DesShow)
+        self.DeDesButton = Button(self,text='Decode Des', command=self.DeDesShow)
 
     def OutputWidgets(self):
-        self.Output = Label(self,textvariable =self.Caesartext)
+        self.OutputCaesar = Label(self,textvariable =self.CaesarText)
+        self.OutputDes = Label(self,textvariable=self.DesText)
+        self.OutputDeDes = Label(self,textvariable=self.DeDesText)
 
-    def Caesar(self):
+    def doCaesar(self,plaintext):
         cipher = ''
-        for i in self.plaintext:
+        for i in plaintext:
             asc = ord(i)
             asc = asc + 3
             if asc >= ord('0') + 3 and asc <= ord('9') + 3:
@@ -43,13 +48,35 @@ class Application(Frame):
         return cipher
 
     def CaesarShow(self):
-        self.plaintext = self.Input.get() or ''
-        self.Casercipher = self.Caesar()
-        self.Caesartext.set(self.Casercipher)
+        plaintext = self.Input.get() or ''
+        Casercipher = self.doCaesar(plaintext)
+        self.CaesarText.set(Casercipher)
+
+    def DesShow(self):
+        plaintext = self.Input.get() or ''
+        k = des.des(b"DESCRYPT", des.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=des.PAD_PKCS5)
+        d = k.encrypt(plaintext)
+        Encrypted = str(d).replace('b','',1)
+        self.DesDecrypted = str(k.decrypt(d)).replace('b','',1)
+        DesCipher = Encrypted
+        self.DesText.set(DesCipher)
+
+    def DeDesShow(self):
+        if self.DesDecrypted == 0:
+            self.DeDesText.set('Des Encryption First')
+        else:
+            self.DeDesText.set(self.DesDecrypted)
+
+
 
     def init(self):
-        self.Caesartext = StringVar()
-        self.Caesartext.set('***')
+        self.DesDecrypted = 0
+        self.CaesarText = StringVar()
+        self.CaesarText.set('***')
+        self.DesText = StringVar()
+        self.DesText.set('***')
+        self.DeDesText = StringVar()
+        self.DeDesText.set('***')
         self.InputWidgets()
         self.OutputWidgets()
 
@@ -62,7 +89,11 @@ class Application(Frame):
         self.hint.pack()
         self.Input.pack()
         self.CaserButton.pack()
-        self.Output.pack()
+        self.OutputCaesar.pack()
+        self.DesButton.pack()
+        self.OutputDes.pack()
+        self.DeDesButton.pack()
+        self.OutputDeDes.pack()
 
 
 app = Application()
--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-k = 'fuck you111'
-for i in k:
-    
+# -*- coding:utf-8 -*-
+import des
+data = "123ssx"
+k = des.des(b"DESCRYPT", des.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=des.PAD_PKCS5)
+d = k.encrypt(data)
+print(str(d).replace('b','',1))
+E = ("Encrypted: " + str(d).replace('b','',1))
+D = ("Decrypted: " + str(k.decrypt(d)).replace('b','',1))
+print (E)
+print (D)
+# assert (k.decrypt(d) == data)
